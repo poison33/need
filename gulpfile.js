7b47411640f8defd68a87d0b1d10bc626711eaa0
poison33 <poison_560@bk.ru>
2017-10-21 11:32:17 +0300

    21

--- gulpfile.js
+++ gulpfile.js
@@ -45,7 +45,7 @@ gulp.task('css', function() {
 });
 
 //перезагрузка страницы в браузере
-gulp.task('watch', ['browser-sync', 'css'], function() {
+gulp.task('watch', ['browser-sync'], function() {
     gulp.watch('app/sass/**/*.sass', ['sass']); // Наблюдение за sass файлами в папке sass
     gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
     gulp.watch('app/js/**/*.js', browserSync.reload);   // Наблюдение за JS файлами в папке js
@@ -100,7 +100,7 @@ gulp.task('end', ['zip'], function () {
 });
 
 //сборка на продакшен,(все готово)
-gulp.task('build', ['css', 'prefix', 'img'], function() {
+gulp.task('build', ['prefix', 'img'], function() {
 
     var buildCss = gulp.src('app/css/**/*') // Переносим CSS стили в продакшен
     .pipe(gulp.dest('dist/css'))
